ab8b59c34ca0d9a4dd23818144a8429f0b091cc0
Yaseen Halabi <yaseenhalabi@gmail.com>
2025-07-26 18:02:08 +0100

    Initial Commit

---
+++ secondthought/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  secondthought
+//
+//  Created by Yaseen Halabi on 7/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ secondthought/secondthoughtApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondthoughtApp.swift
+//  secondthought
+//
+//  Created by Yaseen Halabi on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct secondthoughtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
